484808cd851ffd6fc5c3097086d9b9573d5eb057
hwangs12 <sungjun94@gmail.com>
2024-12-02 06:28:17 -0500

    small update

--- day002part001.c
+++ day002part001.c
@@ -1,2 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+
+parse_input_text() {
+    FILE *file_ptr;
+    char str[50];
+    int list_1[1000];
+    int list_2[1000];
+    int num1;
+    int num2;
+    char* endptr;
+    char* token;
+    int index1 = 0;
+    int index2 = 0;
+    file_ptr = fopen("day001input.txt", "r");
+    if (NULL == file_ptr) {
+        printf("File can't be opened \n");
+    }
+    while (fgets(str, 50, file_ptr) != NULL) {
+        token = strtok(str, "   ");
+        num1 = atoi(token);
+        list_1[index1] = num1;
+        while (token != NULL) {
+            token = strtok(NULL, "   ");
+            if (token != NULL) {
+                num2 = atoi(token);
+                list_2[index2] = num2;
+            }
+        }
+        index1++;
+        index2++;
+    }
+}
+
+int main() {
+
+}
