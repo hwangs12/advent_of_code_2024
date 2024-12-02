61f392cac7b76764d0faaa2623f6fad195f869b5
hwangs12 <sungjun94@gmail.com>
2024-12-02 06:36:55 -0500

    hooray

--- day002part001.c
+++ day002part001.c
@@ -1,7 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-parse_input_text() {
+int all_increasing(int* arr, int arrSize) {
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] < arr[i-1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int all_decreasing(int* arr, int arrSize) {
+    for (int i = 1; i < arrSize; i++) {
+        if (arr[i] > arr[i-1]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int safe_level(int* arr, int arrSize) {
+    int diff;
+    for (int i = 1; i < arrSize; i++) {
+        diff = abs(arr[i] - arr[i-1]);
+        if (diff < 1 || diff > 3) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+void parse_input_text() {
     FILE *file_ptr;
     char str[50];
     int list_1[1000];
