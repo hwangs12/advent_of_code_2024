1adc55755e189e8ece69bb235c59c3bdc24f3834
hwangs12 <sungjun94@gmail.com>
2024-12-02 23:07:46 -0500

    awesome solution

--- day002part001.c
+++ day002part001.c
@@ -31,6 +31,26 @@ int safe_level(int* arr, int arrSize) {
     return 1;
 }
 
+int evaluate_dampener(int* arr, int arrSize, int skipIndex) {
+    int safe = 0;
+    int arrCopy[20];
+    memcpy(arrCopy, arr, sizeof(arrCopy));
+    // remove an element of skipIndex from arr
+    for (int index = skipIndex; index < arrSize; index++) {
+        arrCopy[index] = arrCopy[index+1];
+    }
+
+    if (skipIndex == arrSize - 1) {
+        arrCopy[skipIndex] = 0;
+    }
+
+    // evaluate its safety;
+    safe = (all_decreasing(arrCopy, arrSize - 1) || all_increasing(arrCopy, arrSize - 1)) && safe_level(arrCopy, arrSize - 1);
+    
+    return safe;
+
+}
+
 void parse_input_text() {
     FILE *file_ptr;
     char str[50];
@@ -62,6 +82,14 @@ void parse_input_text() {
         int inc_dec_safe = (all_decreasing(arrToCheck, count) || all_increasing(arrToCheck, count)) && safe_level(arrToCheck, count);
         if (inc_dec_safe) {
             totalSafe++;
+        } else {
+            for (int k = 0; k < count; k++) {
+                inc_dec_safe = evaluate_dampener(arrToCheck, count, k);
+                if (inc_dec_safe) {
+                    totalSafe++;
+                    break;
+                }
+            }
         }
 
         index = 0;
