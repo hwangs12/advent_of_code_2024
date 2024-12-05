5e7e762cb0f0278d39e5f48165f8e7a747ef876f
hwangs12 <sungjun94@gmail.com>
2024-12-05 16:42:26 -0500

    hey hey

--- day005part001.c
+++ day005part001.c
@@ -10,34 +10,50 @@
 #include <string.h>
 
 void read_rulesheet() {
-    FILE *file_ptr;
-    FILE *file_ptr_2;
-    file_ptr = fopen("day005input000_rulesheet.txt", "r");
-    file_ptr_2 = fopen("day005input000_updates.txt", "r");
-    char str[10];
-    char* token1;
-    char* token2;
+    FILE *rulesheet;
+    FILE *update_instruction;
+    rulesheet = fopen("day005input001_rulesheet.txt", "r");
+    update_instruction = fopen("day005input000_updates.txt", "r");
+    char rules[6000];
+    char str[100];
+    char* token;
+    char* savedptr;
     char swap[10] = "";
     char copy1[5];
     char copy2[5];
-    int num1;
-    int num2;
-    while (fgets(str, 10, file_ptr) != NULL) {
-        token1 = strtok(str, "|");
-        token2 = strtok(NULL, "\n");
-        memcpy(copy1, token1, 5);
-        memcpy(copy2, token1, 5);
-        memcpy(swap, token2, 5);
+    int midNumber = 0;
 
-        // when swap is in the rulesheet that update instructions are wrong
-        // when no swap is in the rulesheet that is correct, we can search for the middle value
+    fgets(rules, 6000, rulesheet);
+
+    while (fgets(str, 100, update_instruction) != NULL) {
+        token = strtok_r(str, ",", &savedptr);
+
+        while (token != NULL) {
+            memcpy(copy1, token, 4);
+            token = strtok_r(NULL, ",", &savedptr);
+            if (token == NULL) {
+                break;
+            }
+            memcpy(copy2, token, 4);
+            strcat(swap,copy2);
+            strcat(swap,copy1);
+
+            // compare swap in the rules section
+            printf("%s\n", swap);
+            // if there is swapped item in the rule, then break the loop and get another line
+            // if there is no swapped item in the rule, continue and run the next token and swap;
+
+            
+            memset(swap, 0, sizeof(swap));
+            memset(copy1, 0, sizeof(copy1));
+            memset(copy2, 0, sizeof(copy2));
+        }
+        break;
         
-
-        strcat(copy1, swap);
-        strcat(swap, copy2);
-        printf("%s\n", copy1);
-        printf("-------\n");
     }
+
+    fclose(update_instruction);
+    fclose(rulesheet);
 }
 
 int main() {
