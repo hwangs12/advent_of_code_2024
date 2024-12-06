a77e3d3fa372d92d3ee888a5f1db7c52c8353063
hwangs12 <sungjun94@gmail.com>
2024-12-05 23:13:01 -0500

    update the part

--- day005part001.c
+++ day005part001.c
@@ -16,22 +16,26 @@ void read_rulesheet() {
     update_instruction = fopen("day005input000_updates.txt", "r");
     char rules[6000];
     char str[100];
+    char saved_token[100][5];
     char* token;
+    char* swap_is_here = "";
     char* savedptr;
     char swap[10] = "";
     char copy1[5];
     char copy2[5];
-    int midNumber = 0;
+    int sum = 0;
+    int index = 0;
 
     fgets(rules, 6000, rulesheet);
-
+start_new_line:
     while (fgets(str, 100, update_instruction) != NULL) {
         token = strtok_r(str, ",", &savedptr);
-
         while (token != NULL) {
             memcpy(copy1, token, 4);
-            token = strtok_r(NULL, ",", &savedptr);
+            strcpy(saved_token[index++], copy1);
+            token = strtok_r(NULL, ",\n", &savedptr);
             if (token == NULL) {
+                memset(copy1, 0, sizeof(copy1));
                 break;
             }
             memcpy(copy2, token, 4);
@@ -39,7 +43,16 @@ void read_rulesheet() {
             strcat(swap,copy1);
 
             // compare swap in the rules section
-            printf("%s\n", swap);
+            swap_is_here = strstr(rules, swap);
+            if (swap_is_here) {
+                memset(swap, 0, sizeof(swap));
+                memset(copy1, 0, sizeof(copy1));
+                memset(copy2, 0, sizeof(copy2));
+                memset(saved_token, 0, sizeof(saved_token));
+                index = 0;
+                goto start_new_line;
+            }
+
             // if there is swapped item in the rule, then break the loop and get another line
             // if there is no swapped item in the rule, continue and run the next token and swap;
 
@@ -48,9 +61,16 @@ void read_rulesheet() {
             memset(copy1, 0, sizeof(copy1));
             memset(copy2, 0, sizeof(copy2));
         }
-        break;
-        
+
+        sum += atoi(saved_token[index/2]);
+        if (swap_is_here) {
+            memset(swap_is_here, 0, sizeof(char*));
+        }
+        memset(saved_token, 0, sizeof(saved_token));
+        index = 0;
     }
+
+    printf("%d", sum);
 
     fclose(update_instruction);
     fclose(rulesheet);
