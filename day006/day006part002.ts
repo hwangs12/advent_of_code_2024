fbbc1fcd464314b9eecbffdeb5ebec1d1ad5060d
hwangs12 <sungjun94@gmail.com>
2024-12-12 21:25:51 -0500

    haha

--- day006/day006part002.ts
+++ day006/day006part002.ts
@@ -21,49 +21,82 @@ async function readFileAndParseTo(filename: string, parseTo: ParsingTypes) {
     }
 }
 
-function exited(matrix: string[][], horseLocation: number[], direction: string) {
-    let row = horseLocation[0];
-    let column = horseLocation[1];
-    return (direction === "left" && column === 0) || (direction === "right" && column === matrix[0].length - 1) || (direction === "up" && row === 0) || (direction === "down" && row === matrix.length);
+function exited(matrix: string[][], horseLocation: number[]) {
+    const row = horseLocation[0];
+    const column = horseLocation[1];
+    const cur = matrix[row][column];
+    return (cur === '<' && column === 0) || (cur === '>' && column === matrix[0].length - 1) || (cur === "^" && row === 0) || (cur === "v" && row === matrix.length - 1);
+}
+
+function keepMoving(matrix: string[][], horseLocation: number[]) {
+    const row = horseLocation[0];
+    const column = horseLocation[1];
+    const cur = matrix[row][column];
+    const left = matrix[row][column - 1];
+    const right = matrix[row][column + 1];
+    const up = matrix[row - 1][column];
+    const down = matrix[row + 1][column];
+    if (cur === '>' && right !== '#') {
+        matrix[row][column] = '.'
+        matrix[row][column+1] = '>'
+    } else if (cur === '^' && up !== '#') {
+        matrix[row][column] = '.'
+        matrix[row-1][column] = '^'
+    } else if (cur === '<' && left !== '#') {
+        matrix[row][column] = '.'
+        matrix[row][column-1] = '<'
+    } else if (cur === 'v' && down !== '#') {
+        matrix[row][column] = '.'
+        matrix[row+1][column] = 'v'
+    }
 }
 
 function moveUp(matrix: string[][], horseLocation: number[]): void {
     const row = horseLocation[0];
     const column = horseLocation[1];
-    matrix[row][column] = '.';
+    matrix[row][column] = '+';
     matrix[row - 1][column] = '^';
 }
 
 function moveDown(matrix: string[][], horseLocation: number[]): void {
     const row = horseLocation[0];
     const column = horseLocation[1];
-    matrix[row][column] = '.';
-    matrix[row + 1][column] = '^';
+    matrix[row][column] = '+';
+    matrix[row + 1][column] = 'v';
 }
 
 function moveLeft(matrix: string[][], horseLocation: number[]): void {
     const row = horseLocation[0];
     const column = horseLocation[1];
-    matrix[row][column] = '.';
-    matrix[row][column - 1] = '^';
+    matrix[row][column] = '+';
+    matrix[row][column - 1] = '<';
 }
 
 function moveRight(matrix: string[][], horseLocation: number[]): void {
     const row = horseLocation[0];
     const column = horseLocation[1];
-    matrix[row][column] = '.';
-    matrix[row][column + 1] = '^';
+    matrix[row][column] = '+';
+    matrix[row][column + 1] = '>';
 }
 
-function hitWallAndMove(matrix: string[][], horseLocation: number[]): void{
+function hitWallOrMove(matrix: string[][], horseLocation: number[]): void{
     const row = horseLocation[0];
     const column = horseLocation[1];
+    const cur = matrix[row][column];
     const left = matrix[row][column - 1];
     const right = matrix[row][column + 1];
     const up = matrix[row - 1][column];
     const down = matrix[row + 1][column];
-    if (up === '#' && matrix[down[0]][down]) {
-
+    if (up === '#' && cur === '^') {
+        moveRight(matrix, horseLocation);
+    } else if (left === '#' && cur === '<') {
+        moveUp(matrix, horseLocation);
+    } else if (right === '#' && cur === '>') {
+        moveDown(matrix, horseLocation);
+    } else if (down === '#' && cur === 'v') {
+        moveLeft(matrix, horseLocation);
+    } else {
+        keepMoving(matrix, horseLocation);
     }
 }
 
@@ -73,8 +106,17 @@ function horseLooped(matrix: string[][], startingPoint: number[]): boolean {
     return false;
 }
 
+function runTheGame(matrix: string[][], horseLocation: number[]) {
+    let move = 1;
+    while (!exited(matrix, horseLocation)) {
+        hitWallOrMove(matrix, horseLocation);
+        move++;
+    }
+    console.log(move);
+}
+
 async function analyzeMatrix() {
-    const playBoard = await readFileAndParseTo("./day006/day006input000.txt", ParsingTypes.Matrix);
+    const playBoard = await readFileAndParseTo("./day006input000.txt", ParsingTypes.Matrix);
     let initialLocation = [0, 0]; // row and column;
     if (playBoard) {
         for (let i = 0; i < playBoard.length; i++) {
@@ -83,7 +125,9 @@ async function analyzeMatrix() {
                 initialLocation[1] = playBoard[i].indexOf("^");
             }
         }
+        runTheGame(playBoard, initialLocation)
     }
-}
+
+;}
 
 analyzeMatrix();
