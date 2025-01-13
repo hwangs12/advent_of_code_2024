f15a3e7a6033a6a3542c8feb0a615c5e50478a93
hwangs12 <sungjun94@gmail.com>
2025-01-12 21:53:17 -0500

    possible use of recursion

--- day010/part1.ts
+++ day010/part1.ts
@@ -1,9 +1,13 @@
 import fs from 'fs'
 
 class Solution {
+    private fullmap: number[][] = [];
     private trailheads: number[][] = [];
     private summits: number[][] = [];
     private trailCandidates: Record<string, number[][]> = {};
+    private hikingTrails: number[][] = [];
+    private rowMax = 0;
+    private colMax = 0;
     
     private fileToArray(filename: string) {
         return fs.readFileSync(`./${filename}`, 'utf8').split('\n').map(line => {
@@ -12,7 +16,9 @@ class Solution {
     }
 
     private convertStrNumToNumber(fileArr: string[][]) {
+        this.rowMax = fileArr.length - 1;
         return fileArr.map((row) => {
+            if (!this.colMax) this.colMax = row.length - 1;
             return row.map((str) => {
                 return Number(str);
             })
@@ -39,25 +45,65 @@ class Solution {
         }
     }
 
-    private hikingCandidateMap(trailheadMap: number[][], summitMap: number[][]) {
+    private mapHikingCandidates(trailheadMap: number[][], summitMap: number[][]) {
         for (const trailhead of trailheadMap) {
             const [row, col] = trailhead;
+            this.trailCandidates[JSON.stringify(trailhead)] = [];
             for (const summit of summitMap) {
-                if (Math.abs(row - summit[0]) < 9 && Math.abs(col - summit[1])) {
-
-                }
+                if (Math.abs(row - summit[0]) + Math.abs(col - summit[1]) <= 9) {
+                    this.trailCandidates[JSON.stringify(trailhead)].push(summit)
+                } 
             }
+        }
+        return this.trailCandidates;
+    }
+
+    private isHikingTrail(trailhead: number[]) {
+        const [row, col] = trailhead;
+        const uphill = this.fullmap[row][col] + 1;
+        const down = row < this.rowMax ? this.fullmap[row+1][col] : null
+        const up = row > 0 ? this.fullmap[row-1][col] : null
+        const right = col < this.colMax ? this.fullmap[row][col+1] : null;
+        const left = col > 0 ? this.fullmap[row][col-1] : null;
+        if (down && down === uphill) {
+            if (uphill === 9) {
+                this.hikingTrails.push([row+1, col])
+            } else {
+                this.isHikingTrail([row+1, col])
+            }
+        } else if (up && up === uphill) {
+            if (uphill === 9) {
+                this.hikingTrails.push([row-1, col])
+            } else {
+                this.isHikingTrail([row-1, col])
+            }
+        } else if (right && right === uphill) {
+            if (uphill === 9) {
+                this.hikingTrails.push([row, col+1])
+            } else {
+                this.isHikingTrail([row, col+1])
+            }
+        } else if (left && left === uphill) {
+            if (uphill === 9) {
+                this.hikingTrails.push([row, col-1])
+            } else {
+                this.isHikingTrail([row, col-1])
+            }
+        } else {
+            return false;
         }
     }
 
     public solve() {
         // convert file to num matrix;s
-        const numArr = this.fileToNumArray('sample.txt');
+        this.fullmap = this.fileToNumArray('sample.txt');
         
         // update coordinates 
-        this.getTrailheadsAndSummits(numArr);
-        console.log(this.trailheads);
-        console.log(this.summits);
+        this.getTrailheadsAndSummits(this.fullmap);
+
+        this.mapHikingCandidates(this.trailheads, this.summits)
+
+        this.isHikingTrail([0, 2]);
     }
 }
 
