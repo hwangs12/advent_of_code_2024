c87f7f516bdd2822902662033b1fb42058dea026
hwangs12 <sungjun94@gmail.com>
2025-01-22 22:10:54 -0500

    holy

--- day012/part1.ts
+++ day012/part1.ts
@@ -1,12 +1,19 @@
-import fs from 'fs'
+import fs from "fs";
 
 class Solution {
-    private map: string[][] = []
-    private letterCoordinates: Record<string, number[]> = {}
+    private map: string[][] = [];
+    private maxRow: number = 0;
+    private maxCol: number = 0;
+    private letterCoordinates: Record<string, number[]> = {};
+    private pathTrackingRoutes: number[];
+    private visited: number[] = [];
     private fileToArray(filename: string) {
-       return fs.readFileSync(filename, 'utf8').split('\n').map((line) => {
-            return line.split('')
-        });
+        return fs
+            .readFileSync(filename, "utf8")
+            .split("\n")
+            .map((line) => {
+                return line.split("");
+            });
     }
 
     private convertCoordinateToNumber(row: number, col: number) {
@@ -14,46 +21,79 @@ class Solution {
     }
 
     private convertNumberToCoordinate(num: number) {
-        return [Math.floor(num/1000), num % 1000]
+        return [Math.floor(num / 1000), num % 1000];
     }
 
-    private neighborIsSame(row: number, col: number): boolean {
-        const coordinateUnderInvestigation = this.map[row][col]
-        const up = this.map[row-1][col]
-        const right = this.map[row][col+1]
-        const down
-        const left
+    public IsOnNewIsland(row: number, col: number): boolean {
+        const coordinateUnderInvestigation = this.map[row][col];
+        const up = row > 0 ? this.map[row - 1][col] : null;
+        const right = col < this.maxCol - 1 ? this.map[row][col + 1] : null;
+        const down = row < this.maxRow - 1 ? this.map[row + 1][col] : null;
+        const left = col > 0 ? this.map[row][col - 1] : null;
+        if (coordinateUnderInvestigation === up) {
+            this.pathTrackingRoutes;
+        }
+
+        return coordinateUnderInvestigation !== up && coordinateUnderInvestigation !== left;
+    }
+
+    public getArea(row: number, col: number): number {
+        const current = this.map[row][col];
+        this.visited.push(this.convertCoordinateToNumber(row, col));
+        const up = row > 0 ? this.map[row - 1][col] : null;
+        const right = col < this.maxCol - 1 ? this.map[row][col + 1] : null;
+        const down = row < this.maxRow - 1 ? this.map[row + 1][col] : null;
+        const left = col > 0 ? this.map[row][col - 1] : null;
+        if (
+            (this.visited.includes(this.convertCoordinateToNumber(row - 1, col)) || current !== up) &&
+            (this.visited.includes(this.convertCoordinateToNumber(row, col + 1)) || current !== right) &&
+            (this.visited.includes(this.convertCoordinateToNumber(row + 1, col)) || current !== down) &&
+            (this.visited.includes(this.convertCoordinateToNumber(row, col - 1)) || current !== left)
+        ) {
+            return 1;
+        }
+        // surrounding config:
+        if (current === up && !this.visited.includes(this.convertCoordinateToNumber(row - 1, col))) {
+            return 1 + this.getArea(row - 1, col);
+        } 
+        
+        if (current === right && !this.visited.includes(this.convertCoordinateToNumber(row, col + 1))) {
+            return 1 + this.getArea(row, col + 1);
+        } 
+        
+        if (current === down && !this.visited.includes(this.convertCoordinateToNumber(row + 1, col))) {
+            return 1 + this.getArea(row + 1, col);
+        } 
+        
+        if (current === left && !this.visited.includes(this.convertCoordinateToNumber(row, col - 1))) {
+            return 1 + this.getArea(row, col - 1);
+        } 
+        return 0;
     }
 
     /* Collection of each letter coordinate first appearing in the loop from left to right, top to bottom  */
     private collectLetterCoordinates(areaMap: string[][]) {
-        for (let row=0; row < areaMap.length; row++) {
-            for (let col=0; col < areaMap[row].length; col++) {
-                if (this.letterCoordinates[areaMap[row][col]]) {
-                    if (this.neighborIsSame(row, col)) {
-
-                    }
+        for (let row = 0; row < areaMap.length; row++) {
+            for (let col = 0; col < areaMap[row].length; col++) {
+                if (this.letterCoordinates[areaMap[row][col]] && this.letterCoordinates[areaMap[row][col]].length > 0) {
                     continue;
-                } 
-                this.letterCoordinates[areaMap[row][col]] = this.convertCoordinateToNumber(row, col);
+                }
+                this.letterCoordinates[areaMap[row][col]] = [this.convertCoordinateToNumber(row, col)];
             }
         }
     }
 
-    private getArea(startingCoordinate: number) {
-
-    }
-
-    private getPerimeter(startingCoordinate: number) {
-
-    }
+    private getPerimeter(startingCoordinate: number) {}
 
     public solve() {
-        this.map = this.fileToArray('sample.txt');
+        this.map = this.fileToArray("sample.txt");
+        this.maxRow = this.map.length - 1;
+        this.maxCol = this.map[0].length - 1;
         this.collectLetterCoordinates(this.map);
-        console.log(this.letterCoordinates)
+        const area = this.getArea(0, 0);
+        console.log(area);
     }
 }
 
-const sol = new Solution()
-console.log(sol.solve())
+const sol = new Solution();
+sol.solve();
